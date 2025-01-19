877af61d6198f21b55219fb49bd2a428f41eb121
Johan <johanvilla64@gmail.com>
2025-01-19 15:27:28 -0500

    feat: :sparkles: enhance DishReview and RestaurantReview models with detailed rating management (taste, presentation, service, etc) and toString methods

--- app/src/main/java/org/example/models/DishReview.java
+++ app/src/main/java/org/example/models/DishReview.java
@@ -1,7 +1,43 @@
 package org.example.models;
 
 public class DishReview extends Review {
-  public DishReview(String comment, float rating) {
-    super(comment, rating);
+  private Float presentationRating;
+  private Float tasteRating;
+
+  public DishReview(String comment, Float presentationRating, Float tasteRating) {
+    super(comment);
+    this.presentationRating = presentationRating;
+    this.tasteRating = tasteRating;
+    calculateRating();
   }
+
+  @Override
+  public void calculateRating() {
+    Float average = (tasteRating + presentationRating) / 2;
+    this.setAverageRating(Math.round(average * 100.0) / 100.0F);
+  }
+
+  @Override
+  public String toString() {
+    return "Review de plato: " +
+            "\n    Presentacion: " + presentationRating +
+            "\n    Sabor: " + tasteRating;
+  }
+
+  public Float getPresentationRating() {
+    return presentationRating;
+  }
+
+  public void setPresentationRating(Float presentationRating) {
+    this.presentationRating = presentationRating;
+  }
+
+  public Float getTasteRating() {
+    return tasteRating;
+  }
+
+  public void setTasteRating(Float tasteRating) {
+    this.tasteRating = tasteRating;
+  }
+
 }
