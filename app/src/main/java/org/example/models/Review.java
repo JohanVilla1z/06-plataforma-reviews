bdf209aa07d3294e96f7085c00e79a2e2368a389
Johan <johanvilla64@gmail.com>
2025-01-19 15:26:14 -0500

    feat: :sparkles: add review ID generation and average rating management in Review model

--- app/src/main/java/org/example/models/Review.java
+++ app/src/main/java/org/example/models/Review.java
@@ -1,24 +1,53 @@
 package org.example.models;
 
 public abstract class Review {
+  protected Integer reviewId;
   protected String comment;
-  protected Float rating;
+  protected Float averageRating;
 
-  public Review(String comment, Float rating) {
+  protected static Integer reviewCount = 0;
+
+  public Review(String comment) {
+    this.reviewId = generateId();
     this.comment = comment;
-    this.rating = rating;
+    this.averageRating = 0f;
   }
 
-  public Float getRating() {
-    return rating;
+  private Integer generateId() {
+    return reviewCount++;
+  }
+
+  public void setAverageRating(Float averageRating) {
+    this.averageRating = averageRating;
+  }
+
+  public Integer getReviewId() {
+    return reviewId;
+  }
+
+  public void setReviewId(Integer reviewId) {
+    this.reviewId = reviewId;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public static Integer getReviewCount() {
+    return reviewCount;
+  }
+
+  public static void setReviewCount(Integer reviewCount) {
+    Review.reviewCount = reviewCount;
+  }
+
+  public Float getAverageRating() {
+    return averageRating;
   }
 
   public String getComment() {
     return comment;
   }
 
-  @Override
-  public String toString() {
-    return "Rating: " + rating + " - " + comment;
-  }
+  public abstract void calculateRating();
 }
